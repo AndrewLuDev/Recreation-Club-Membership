496d531febf87d9c20a8f7d144781b224145589b
Andrew Lu <94149647+AndrewLuDev@users.noreply.github.com>
2022-04-14 22:02:30 +0000

    Implemented the following: - Coaches can now update member information (name/phone number/address/number of sessions unpaid) - Allowed coaches to send a personalized message to all or specific members in the club - Created user interface for Treasurers - Members and Coaches can now view the scheduled sessions for the month

--- main.py
+++ main.py
@@ -15,7 +15,9 @@ class accountInfo:
     self.paid = paid          # number of paid sessions in a row
     self.unpaid = unpaid
     self.address = address
-
+    
+#####################################################################################
+    
 def printTitle():
   print("RECREATION CLUB MEMBERSHIP")
   print("--------------------------")
@@ -24,15 +26,23 @@ def printSelections(selections):
   selectionNum = 0  
   for select in selections:
     selectionNum += 1
-    print(str(selectionNum) + ". " + str(select))
+    print(f"{selectionNum}. {select}")
 
+def clearAll():
+  #deletes all info from database
+  for key in db.keys():
+    del db[key]
 
+#####################################################################################
+    
 def main():
     #clears the database
     #clearAll()
 
     if "admin" not in db.keys():
       db["admin"] = ["admin", "password", "Coach", "Coach123", "6490001234", 0, 0, "123 Address St."]
+    if "admin2" not in db.keys():
+      db["admin2"] = ["admin2", "password", "Treasurer", "Treasurer123", "4160001234", 0, 0, "123 Address St."]
   
     for user in db.keys():
       #db[user][0]
@@ -42,8 +52,9 @@ def main():
       #loginInfo[username] = password
       loginInfo[db[user][0]] = db[user][1]
 
-      
     mainMenu(loginInfo)
+  
+#####################################################################################
 
 def mainMenu(loginInfo):
   selection = ''
@@ -60,164 +71,14 @@ def mainMenu(loginInfo):
       login('')
     elif selection == '3' or selection.lower() == 'q':
       print("Quitting")
+      #exit()
     else:
       os.system('clear')
-      mainMenu(loginInfo)
       print("Invalid selection.\n")
-
-def memberScreen(user):
-  selections = ["Schedules", "Your Bookings", "Make a Payment", "Logout" ]
-  selection = ''
-  while selection.lower() != 'q':
-    printTitle()
-    print("LOGGED IN AS: MEMBER")
-    printSelections(selections)
-
-    selection = input("Selection Number: ")
-
-    if selection == '1':
-      listSchedule()
-    elif selection == '2':
-      for item in allAccounts:
-        if item.username == user:
-          print("you have 0 bookings")
-    elif selection == '3':
-      printTitle()
-      print("LOGGED IN AS: MEMBER")
-      makePayment(user)
-
-def listMembers():
-  print("Listing All Members:\n")
-  ##############################################
-  ###############tabulate this later############
-  ##############################################
-  #Format: Name, Phone number, number of paid classes in a row, number of unpaid classes
-  numMembers = 0
-  memberList = []
-  for username in db.keys():
-    if db[username][2] == "Member":
-      numMembers += 1
-      memberList.append(username)
-      #print Member's name, phone number, paid classes in a row and unpaid classes
-      print(str(numMembers) + ".", db[username][3], db[username][4], db[username][5], db[username][6])
-    else:
-      #debugging: prints whoever else is in the database (ie. coaches/treasurers)
-      print("somethingsomething",db[username][3], db[username][4], db[username][5], db[username][6])
-      
-####################################################################################
-def modifySpecificMember(user, username):
-  
-  descNum = input("\nWhich description do you wish to modify?\nEnter the description number: ")
-
-  if int(descNum) < 1 or int(descNum) > 5:
-    print("Invalid description nubmer. Please enter a number from 1 to 5")
-  else:
-    if descNum == '1':
-      db[username][3] = input("Please enter the member's First and Last name: \n")
-    elif descNum == '2':
-      db[username][4] = input("Please enter the member's phone number: \n")
-    elif descNum == '3':
-      db[username][5] = input("Please enter the member's number of paid classes in a row")
-    elif descNum == '4':
-      db[username][6] = input("Please enter the member's number of unpaid classes: \n")
-    elif descNum == '5':
-      db[username][7] = input("Please enter the member's address: \n")
-    print("\nThe member's information has been updated.")
-
-  contModify = input("\nContinue modifying this member? Please input Y/N \nOption: ")
-
-  while contModify.lower() != 'n':
-    if contModify.lower() == 'y':
-      modifySpecificMember(user, username)
-    else:
-      contModify = input("Invalid option. Please type Y or N\nOption: ")
-      
-
-  os.system('clear')
-  printTitle()
-  listMembers()
-  modifyMembers(user)
-  
-#####################################################################################
-
-def modifyMembers(user):
-  numMembers = 0
-  memberList = []
-  for username in db.keys():
-    if db[username][2] == "Member":
-      numMembers += 1
-      memberList.append(username)
-
-  userNum = int(input("\nModify Member number: "))
-        
-  #(name, phone, paid, unpaid, address)
-  username = memberList[userNum-1]
-  printSelections([db[username][3], db[username][4], db[username][5], db[username][6], db[username][7]])
-
-  modifySpecificMember(user, username)
+      mainMenu(loginInfo)
 
 #####################################################################################
-
-def coachScreen(user):
-  selections = ["Schedules", "View Members List", "Send Reminders", "Logout"]
-  selection = ''
-
-
-  os.system('clear')
-  printTitle()
-  print("LOGGED IN AS: COACH")
-  printSelections(selections)
-  
-  while selection.lower() != 'q':
-
-    selection = input ("Selection Number: ")
-
-    while selection.lower() != 'b':
-      if selection == '1':
-        listSchedule()
-      elif selection == '2':
-        listMembers()
-        selection = input("\n'M' to Modify Members\n'B' Back to Menu\nOption: ")
-        
-        os.system('clear')
-        printTitle()
-        if selection.lower() == 'm':
-          listMembers()
-          modifyMembers(user)
-        elif selection.lower() == 'b':
-          coachScreen(user)
-        else:
-          print("Invalid Selection.\n")
-      elif selection == '3':
-        sendReminders()
-      elif selection == '4' or selection.lower() == 'q':
-        print("Logging out.\n")
-        mainMenu(loginInfo)
-        
-
-        
-
-        
-def makePayment(user):
-  os.system('clear')
-  selections = ["Paypal", "Credit Card", "Bitcoin", "Cancel Payment"]
-  print("200 classes have not yet been paid for")
-  printSelections(selections)
-  
-  selection = input("Selection Number: ")
-  if selection == '1':
-    print("Paypal payment option is currently unavailable. Please try again later\n")
-  elif selection == '2':
-    print("Credit Card payment option is currently unavailable. Please try again later\n")
-  elif selection == '3':
-    print("Bitcoin payment option is currently unavailable. Please try again later\n")
-  elif selection == '4' or selection.lower() == 'q':
-    print("Cancelling payment.")
-    os.system('clear')
-    memberScreen(user)
-  makePayment(user)
-
-
+      
 def register(loginInfo):
   username = input("Enter a username: ")
   while username in loginInfo.keys():  
@@ -253,10 +114,9 @@ def register(loginInfo):
     print(db[user][0], db[user][1])
   print()
 
+#####################################################################################
 
 def login(username):
-  #printTitle()
-  #print("")
   if username != '':
     pass
   else:
@@ -277,23 +137,313 @@ def login(username):
     password = getpass.getpass(prompt="Enter password: ")
   
     if loginInfo[username] == password:
-        #success case
-        print(f"\nLOGGED IN: Hello {username}.\n")
-        for tempUser in allAccounts:
-          if tempUser.username == username:
-            if tempUser.role == "Member":
-              memberScreen(username)
-            elif tempUser.role == "Coach":
-              coachScreen(username)
-            elif tempUser.role == "Treasurer":
-              treasurerScreen(username)
+      print(f"\nLOGGED IN: Hello {username}.\n")
+      for tempUser in allAccounts:
+        if tempUser.username == username:
+          if tempUser.role == "Member":
+            memberScreen(username)
+          elif tempUser.role == "Coach":
+            coachScreen(username)
+          elif tempUser.role == "Treasurer":
+            treasurerScreen(username)
     else:
-        print(f"LOGIN FAILED: {username} is not registered or password is incorrect")
+        #print(f"LOGIN FAILED: {username} is not registered or password is incorrect")
+        print("LOGIN FAILED: password is incorrect")
 
-def clearAll():
-  #deletes all info from database
-  for key in db.keys():
-    del db[key]
+#####################################################################################
+
+def memberScreen(user):
+  selections = ["Schedules", "Your Bookings", "Make a Payment", "Logout" ]
+  selection = ''
+  while selection.lower() != 'q':
+    printTitle()
+    print("LOGGED IN AS: MEMBER")
+    printSelections(selections)
+
+    selection = input("Selection Number: ")
+    print()
+    
+    if selection == '1':
+      listSchedule(user)
+    elif selection == '2':
+      ############################################
+      #########May remove Bookings selection######
+      for item in allAccounts:
+        if item.username == user:
+          print("you have 0 bookings")
+      #############################################
+    elif selection == '3':
+      printTitle()
+      print("LOGGED IN AS: MEMBER")
+      makePayment(user)
+
+#####################################################################################
+
+def listMembers(user):
+  print("Listing All Members:\n")
+  ##############################################
+  ###############tabulate this##################
+  ##############################################
+  #Format: Name, Phone number, number of paid classes in a row, number of unpaid classes
+  numMembers = 0
+  memberList = []
+  for username in db.keys():
+    if db[username][2] == "Member":
+      numMembers += 1
+      memberList.append(username)
+      #print Member's name, phone number, address, paid classes in a row and unpaid classes
+      print(str(numMembers) + ".", db[username][3], db[username][4], db[username][7], db[username][5], db[username][6])
+    else:
+      #debugging: prints whoever else is in the database (ie. coaches/treasurers)
+      print("somethingsomething", db[username][3], db[username][4], db[username][7], db[username][5], db[username][6])
+
+        
+####################################################################################
+def modifySpecificMember(user, username):
+  
+  descNum = input("\nWhich description do you wish to modify?\nEnter the description number: ")
+
+  if int(descNum) < 1 or int(descNum) > 5:
+    print("Invalid description nubmer. Please enter a number from 1 to 5")
+  else:
+    if descNum == '1':
+      db[username][3] = input("Please enter the member's First and Last name: \n")
+    elif descNum == '2':
+      db[username][4] = input("Please enter the member's phone number: \n")
+    elif descNum == '3':
+      db[username][7] = input("Please enter the member's address: \n")
+    elif descNum == '4':
+      db[username][5] = input("Please enter the member's number of paid classes in a row: \n")
+    elif descNum == '5':
+      db[username][6] = input("Please enter the member's number of unpaid classes: \n")
+    print("\nThe member's information has been updated.")
+
+  contModify = input("\nContinue modifying this member? Please input Y/N \nOption: ")
+
+  while contModify.lower() != 'n':
+    if contModify.lower() == 'y':
+      modifySpecificMember(user, username)
+    else:
+      contModify = input("Invalid option. Please type Y or N\nOption: ")
+      
+
+  os.system('clear')
+  printTitle()
+  listMembers(user)
+  modifyMembers(user)
+  
+#####################################################################################
+
+def modifyMembers(user):
+  numMembers = 0
+  memberList = []
+  for username in db.keys():
+    if db[username][2] == "Member":
+      numMembers += 1
+      memberList.append(username)
+
+  userNum = input("\nModify Member Number OR type 'B' to return Back to menu: ")
+
+  if userNum.lower() == 'b':
+    coachScreen(user)
+
+  userNum = int(userNum)
+        
+  #(name, phone, address, paid, unpaid)
+  username = memberList[userNum-1]
+  printSelections([db[username][3], db[username][4], db[username][7], db[username][5], db[username][6]])
+
+  modifySpecificMember(user, username)
+
+#####################################################################################
+
+def coachScreen(user):
+  selections = ["Schedules", "View Members List", "Send Reminders", "Logout"]
+  selection = ''
+
+
+  os.system('clear')
+  printTitle()
+  print("LOGGED IN AS: COACH")
+  printSelections(selections)
+  
+  while selection.lower() != 'q':
+
+    selection = input ("Selection Number: ")
+
+    while selection.lower() != 'b':
+      if selection == '1':
+        listSchedule(user)
+      elif selection == '2':
+        listMembers(user)
+        
+        selection = input("\n'M' to Modify Members\n'B' Back to Menu\nOption: ")
+        while selection.lower() != 'm' and selection.lower() != 'b':
+          
+          print("Invalid Selection.\n")
+          selection = input("\n'M' to Modify Members\n'B' Back to Menu\nOption: ")
+        if selection.lower() == 'm':
+          #listMembers(user)
+          modifyMembers(user)
+        elif selection.lower() == 'b':
+          coachScreen(user)
+          
+      elif selection == '3':
+        sendReminders(user)
+      elif selection == '4' or selection.lower() == 'q':
+        print("Logging out.\n")
+        mainMenu(loginInfo)
+    if selection.lower() != 'b' and selection.lower() != 'm' and selection != '1' and selection != '2' and selection != '3' and selection != '4':
+      print("Invalid selection.")
+      selection = input("Enter a number (1 to 4): ")
+
+######################################################################################
+def listSchedule(user):
+  os.system('clear')
+  printTitle()
+  for i in range(1, 31, 3):
+    print(f"Apr {i} ")
+
+  selection = input("Press any key to go back to main menu: ")
+
+  for tempUser in allAccounts:
+    if tempUser.username == user:
+      if tempUser.role == "Member":
+        memberScreen(user)
+      elif tempUser.role == "Coach":
+        coachScreen(user)
+      elif tempUser.role == "Treasurer":
+        treasurerScreen(user)
+
+
+######################################################################################
+
+def sendReminders(user):
+  os.system('clear')
+  printTitle()
+  print("LOGGED IN AS: COACH")
+  listMembers(user)
+
+  print("\nPlease type 'all' if you wish to send a reminder to all the members in the list\nOr list the members you wish to send a reminder to, separated by spaces.\nFor example: 1 3 5 8\n")
+  
+  selections = input("Send to: ")
+  message = input("Reminder Message: ")
+  
+  if selections.lower() == 'all':
+    print(f"\nThe following message has been sent to all members:\n{message}")
+  else:
+    print(f"\nThe following messages have been sent to member numbers: {selections}\n{message}")
+
+  selections = input("\nWould you like to send another message?\nPlease input Y/N: ")
+
+  while selections.lower() != 'y' and selections.lower() != 'n':
+    print("Invalid selection number.")
+    selections = input("\nWould you like to send another message?\nPlease input Y/N: ")
+    
+  if selections.lower() == 'y':
+    sendReminders(user)
+  elif selections.lower() == 'n':
+    coachScreen(user)
+
+######################################################################################
+
+def treasurerScreen(user):
+  selections = ["Schedules", "View Logs"]
+  selection = ''
+  while selection.lower() != 'q':
+    printTitle()
+    print("LOGGED IN AS: TREASURER")
+    printSelections(selections)
+  
+    selection = input("Selection Number: ")
+    print()
+    
+    if selection == '1':
+      listSchedule(user)
+    elif selection == '2':
+      viewLogs(user)
+    elif selection == '3':
+      printTitle()
+      print("LOGGED IN AS: MEMBER")
+      makePayment(user)
+
+######################################################################################
+      
+def viewLogs(user):
+  selections = ["Sort by paid", "Sort by unpaid"]
+
+  os.system('clear')
+  printTitle()
+  print("LOGGED IN AS: TREASURER")
+  printSelections(selections)
+  
+  selection = input("Selection Number: ")
+  
+  print()
+  
+  numMembers = 0
+  memberList = []
+  for username in db.keys():
+    if db[username][2] == "Member":
+      numMembers += 1
+      memberList.append({'username': db[username][0], 
+                        'password': db[username][1],
+                        'role': db[username][2],
+                        'name': db[username][3],
+                        'phone': db[username][4],
+                        'paid': int(db[username][5]),
+                        'unpaid': int(db[username][6]),
+                        'address': db[username][7],})
+
+      ##########the following print statement is for debugging
+      print(str(numMembers) + ".", db[username][3], db[username][4], db[username][7], db[username][5], db[username][6])
+
+  print()
+  
+  if selection == '1':
+    memberList.sort(key=lambda x: x.get('paid'))
+  elif selection == '2':
+    
+    memberList.sort(key=lambda x: x.get('unpaid'))
+
+
+    
+  ##############################################
+  ###############tabulate this##################
+  ##############################################
+  
+  for username in memberList:
+    print(username.get('name'), username.get('phone'), username.get('address'), username.get('paid'), username.get('unpaid'))
+
+  print()
+  
+  
+######################################################################################
+
+def makePayment(user):
+  os.system('clear')
+  selections = ["Paypal", "Credit Card", "Bitcoin", "Cancel Payment"]
+  print("200 classes have not yet been paid for")
+  printSelections(selections)
+  
+  selection = input("Selection Number: ")
+  if selection == '1':
+    print("Paypal payment option is currently unavailable. Please try again later\n")
+  elif selection == '2':
+    print("Credit Card payment option is currently unavailable. Please try again later\n")
+  elif selection == '3':
+    print("Bitcoin payment option is currently unavailable. Please try again later\n")
+  elif selection == '4' or selection.lower() == 'q':
+    print("Cancelling payment.")
+    os.system('clear')
+    memberScreen(user)
+  makePayment(user)
+
+######################################################################################
+
+  
+#####################################################################################
+
 
 
 main()
